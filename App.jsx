50d7aecdfb6d290a66b860ea19d4fdb827fcca5a
Fastorcito <fabian46823@gmail.com>
2024-01-06 12:51:31 -0500

    courseinfo step5

---
+++ App.jsx
@@ -0,0 +1,29 @@
+[33mcommit 2da271596ba02742491e5c4c52527b4b304dbdff[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m)[m
+Author: Fastorcito <fabian46823@gmail.com>
+Date:   Sat Jan 6 12:26:30 2024 -0500
+
+    courseinfo step3-4
+
+[33mcommit b9fd6bd5a2c6245dec01384eb519ab0b8e8633fc[m
+Author: Fastorcito <fabian46823@gmail.com>
+Date:   Thu Jan 4 10:47:47 2024 -0500
+
+    courseinfo step1-2
+
+[33mcommit 68ddbcd67d6c282572b058cf3a039fe740f7d1d8[m[33m ([m[1;31morigin/main[m[33m)[m
+Author: Fastorcito <fabian46823@gmail.com>
+Date:   Fri Dec 29 17:30:15 2023 -0500
+
+    Diagrams of part 0
+
+[33mcommit efd87c55f82cc8036fc254564f1e4d19a77dfd99[m
+Author: Fastorcito <fabian46823@gmail.com>
+Date:   Fri Dec 29 13:03:26 2023 -0500
+
+    Changes in first Diagram
+
+[33mcommit aff7b5408d832be61c3bc3f26b0e9d488c163b09[m
+Author: Fastorcito <fabian46823@gmail.com>
+Date:   Fri Dec 29 12:58:27 2023 -0500
+
+    First diagram of notes page
