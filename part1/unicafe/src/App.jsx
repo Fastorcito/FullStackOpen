c15ecbe9417e8bb8a889b9391e218d3494c2d94f
Fastorcito <fabian46823@gmail.com>
2024-01-08 17:02:51 -0500

    unicafe step1

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodFeedback = () => {
+    setGood(good + 1)
+  }
+
+  const neutralFeedback = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const badFeedback = () => {
+    setBad(bad + 1)
+  }
+  
+  return (
+    <>
+      <Title text="give feedback"/>
+      <div>
+        <Button onSmash={goodFeedback} text="good"/>
+        <Button onSmash={neutralFeedback} text="neutral"/>
+        <Button onSmash={badFeedback} text="bad"/>
+      </div>
+      <Title text="statistics"/>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </>
+  )
+}
+
+const Button = ({onSmash, text}) => 
+  <button onClick={onSmash}>
+    {text}
+  </button>
+
+
+const Title = ({text}) => <h2>{text}</h2>
+
+export default App
+
